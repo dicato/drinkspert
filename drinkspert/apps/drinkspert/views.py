fc803a727f7714abb71b2c78e257eecf66f2d5c3
Locker537 <locker537@gmail.com>
2012-07-14 00:54:37 -0400

    Add basic example views.
    Add a index (homepage) view, and a 'beers' view.

--- drinkspert/apps/drinkspert/views.py
+++ drinkspert/apps/drinkspert/views.py
@@ -9,3 +9,23 @@ from django.shortcuts import render_to_response
 # local imports
 from apps.drinkspert.models import Person, Beer, Gin
 
+
+def index(request):
+    """
+    The homepage.
+    """
+    return render_to_response('index.html')
+    
+def people(request):
+    """
+    Show all people.
+    """
+    all = Person.objects.all()
+    return render_to_response('people.html', {'people': all})
+
+def beers(request):
+    """
+    Show all beers.
+    """
+    all = Beer.objects.all()
+    return render_to_response('beers.html', {'beers': all})
